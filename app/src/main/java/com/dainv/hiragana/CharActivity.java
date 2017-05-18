1a405ac7f467fac85c07098c68613dca44cb9ef2
Dai Nguyen-Van <dainv1989@hotmail.com>
2017-05-19 00:26:48 +0700

    Implement bottom char scroll view

--- app/src/main/java/com/dainv/hiragana/CharActivity.java
+++ app/src/main/java/com/dainv/hiragana/CharActivity.java
@@ -3,19 +3,20 @@ package com.dainv.hiragana;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class CharActivity extends AppCompatActivity {
 
     private static final int HIRAGANA_CHART = 1;
     private static final int KATAKANA_CHART = 2;
 
-    private static int display_char_type = 0;
-
-    private ListView lvCharScroll;
-
-    private static final String basic_chars[] = {
+    private final String basic_chars[] = {
             "a", "i", "u", "e", "o",
             "ka", "ki", "ku", "ke", "ko",
             "sa", "shi", "su", "se", "so",
@@ -29,6 +30,10 @@ public class CharActivity extends AppCompatActivity {
             "n"
     };
 
+    private static int display_char_type = 0;
+
+    private RecyclerView bottomCharScroll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +42,46 @@ public class CharActivity extends AppCompatActivity {
         Intent intent = getIntent();
         display_char_type = intent.getIntExtra("CHAR_TYPE", 0);
 
-        lvCharScroll = (ListView)findViewById(R.id.listCharsNav);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1, basic_chars);
-        lvCharScroll.setAdapter(adapter);
+        bottomCharScroll = (RecyclerView)findViewById(R.id.bottomCharScroll);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.HORIZONTAL, false); /* horizontal display list */
+        bottomCharScroll.setLayoutManager(layoutManager);
+
+        CharAdapter adapter = new CharAdapter(basic_chars);
+        bottomCharScroll.setAdapter(adapter);
+    }
+
+    public class CharAdapter extends RecyclerView.Adapter<CharAdapter.ViewHolder> {
+        private String[] dataset;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView view;
+            public ViewHolder(TextView view) {
+                super(view);
+                this.view = view;
+            }
+        }
+
+        public CharAdapter(String[] dataset) {
+            this.dataset = dataset;
+        }
+
+        @Override
+        public CharAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            TextView view = (TextView) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.bottom_text_bar, parent, false);
+            ViewHolder viewHolder = new ViewHolder(view);
+            return viewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.view.setText(dataset[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return dataset.length;
+        }
     }
 }
