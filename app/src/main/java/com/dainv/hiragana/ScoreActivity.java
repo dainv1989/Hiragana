f1af8e27e8412a86550c561d2adcc76fbcb33b45
Dai Nguyen <dainv1989@hotmail.com>
2017-06-19 15:28:56 +0700

    update screen layout

--- app/src/main/java/com/dainv/hiragana/ScoreActivity.java
+++ app/src/main/java/com/dainv/hiragana/ScoreActivity.java
@@ -1,13 +1,22 @@
 package com.dainv.hiragana;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.dainv.hiragana.model.JPChar;
 
 public class ScoreActivity extends AppCompatActivity {
 
     private TextView tvScore;
+
+    private ImageView imgReplay;
+    private ImageView imgGoHome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +25,32 @@ public class ScoreActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         tvScore = (TextView)findViewById(R.id.txtTotalScore);
+        imgReplay = (ImageView)findViewById(R.id.imgReplay);
+        imgGoHome = (ImageView)findViewById(R.id.imgGoHome);
+
         int score = intent.getIntExtra("SCORE", 0);
         int total = intent.getIntExtra("TOTAL", 0);
 
         tvScore.setText(score + "/" + total);
+
+        final Context context = this;
+        imgReplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ExcerciseActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                intent.putExtra("QUESTION_TYPE", JPChar.QTYPE_READ_HIRA);
+                context.startActivity(intent);
+            }
+        });
+
+        imgGoHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                context.startActivity(intent);
+            }
+        });
     }
 }
