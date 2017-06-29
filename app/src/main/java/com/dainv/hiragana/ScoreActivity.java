eb0c3faf5420bc46733dedc2bae22a2123eb5c07
Dai Nguyen <dainv1989@hotmail.com>
2017-06-29 17:18:36 +0700

    add flashing activity

--- app/src/main/java/com/dainv/hiragana/ScoreActivity.java
+++ app/src/main/java/com/dainv/hiragana/ScoreActivity.java
@@ -66,7 +66,8 @@ public class ScoreActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                                Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 context.startActivity(intent);
             }
         });
